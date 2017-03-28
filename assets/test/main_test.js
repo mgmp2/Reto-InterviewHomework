27158ddea9cc873942c90e568dde6a30f1cb0504
Miriam Mendoza <miriammp1997@gmail.com>
2017-03-28 12:20:40 -0500

    Agregando test

--- assets/test/main_test.js
+++ assets/test/main_test.js
@@ -1,68 +1,75 @@
 
 
-var agente = [{ id : 0 , tipo: 'fisico', url : 'bjstdmngbgr01.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.2' , directorio : '/var/lib/cruise-agent', recursos : [{id: '0', nombre: 'Ubuntu'},{id: '0', nombre: 'firefox'},{id: '0', nombre: 'core-duo'}]}],
-{ id : 1 , tipo: 'fisico', url : 'bjstdmngbgr02.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.3' , directorio : '/var/lib/cruise-agent', recursos : [{id: '0', nombre: 'Ubuntu'}]}],
-{ id : 2 , tipo: 'fisico', url : 'bjstdmngbgr03.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.4' , directorio : '/var/lib/cruise-agent', recursos : []},
-{ id : 3 , tipo: 'fisico', url : 'bjstdmngbgr04.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.5' , directorio : '/var/lib/cruise-agent', recursos : [{id: '0', nombre: 'Ubuntu'}]}],
-{ id : 4 , tipo: 'virtual', url : 'bjstdmngbgr05.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.6' , directorio : '/var/lib/cruise-agent', recursos : [{id: '0', nombre: 'firefox'}]}],
-{ id : 5 , tipo: 'virtual', url : 'bjstdmngbgr06.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.7' , directorio : '/var/lib/cruise-agent', recursos : [{id: '0', nombre: 'core-duo'}]}],
-{ id : 6 , tipo: 'virtual', url : 'bjstdmngbgr07.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.8' , directorio : '/var/lib/cruise-agent', recursos : [{id: '0', nombre: 'core-duo'}]}]
-];
 
-var mostrarAgentes = function(array){
-  for (var i = 0; i < array.length; i++) {
-    (createHTMLPanel(array[i].id)); //Se envía el id del agentes
-  }
-};
-
-
-var recursos = function(){
+var recurso = function(){
     this.id;
     this.nombre;
+    this.Agente = function (){
+      agente =
+      [{ id : 0 , tipo: 'fisico', url : 'bjstdmngbgr01.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.2' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'},{id: 'R1', nombre: 'firefox'}]},
+      { id : 1 , tipo: 'fisico', url : 'bjstdmngbgr02.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.3' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+      { id : 2 , tipo: 'fisico', url : 'bjstdmngbgr03.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.4' , directorio : '/var/lib/cruise-agent', recursos : []},
+      { id : 3 , tipo: 'fisico', url : 'bjstdmngbgr04.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.5' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+      { id : 4 , tipo: 'virtual', url : 'bjstdmngbgr05.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.6' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'firefox'}]},
+      { id : 5 , tipo: 'virtual', url : 'bjstdmngbgr06.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.7' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]},
+      { id : 6 , tipo: 'virtual', url : 'bjstdmngbgr07.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.8' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]}
+    ];
+  };
+
+    this.mostrarAgentes = function(tipo){
+      if(tipo == 'all'){
+        return agente
+      }
+      else{
+        return agente.filter(e => e.tipo == tipo);
+      }
+    };
     this.agregarRecursos = function(id_agente , nombreR){
-      var nuevoArray = [];
-      nuevoArray = agente[id_agente].recursos.push({id : agente[id_agente].recursos.length , nombre : nombreR}});
-      return nuevoArray;
+      var recurso = nombreR.split(",");
+      recurso.map(function(e,i){
+        agente[id_agente].recursos.push({id : 'R'+agente[id_agente].recursos.length , nombre : e.trim() });
+
+      });
+      return agente[id_agente];
     };
 
-    this.btnAll       = function(array){
-      mostrarAgentes(array);
-    };
-
-    this.btnPhysical  = function(array){
-    mostrarAgentes(array);
-    };
-
-    this.btnVirtual   = function(array){
-      mostrarAgentes(array);
-    };
-
+    // this.btnAll       = function(array){
+    //   mostrarAgentes(array);
+    // };
+    //
+    // this.btnPhysical  = function(array,tipo){
+    // mostrarAgentes(array);
+    // };
+    //
+    // this.btnVirtual   = function(array){
+    //   mostrarAgentes(array);
+    // };
+    //
     this.summary      = function (clase, arrayObj){
         var total = 0;
-        arrayObj.map(function (e) {
-          if(e.tipo == clase){
-            total++;
-          }
-        });
-        return total;
+      // console.log(clase);
+        var filtro = arrayObj.filter(e => e.estado == clase);
+        return filtro.length;
     };
-
-    this.mostrarRecursos = function(array indice) {
-        var mostrar = [];
-        var descripcion = agente[indice].url + agente[indice].estado + agente[indice].direccionIP + agente[indice].directorio;
-        array[indice].recursos.map(function(e){
-          mostrar.push(e);
-        });
-        return mostrar;
-    };
-
-
-    this.eliminarRecursos = function (id_agente){
-        var eliminar = agente.map(e => e.id).
-        })
-        agente[id_agente].recursos.splice(eliminar,1);
-        localStorage.setItem('agentes',JSON.stringify(agente));
-      });
+    //
+    // this.mostrarRecursos = function(array indice) {
+    //     var mostrar = [];
+    //     var descripcion = agente[indice].url + agente[indice].estado + agente[indice].direccionIP + agente[indice].directorio;
+    //     array[indice].recursos.map(function(e){
+    //       mostrar.push(e);
+    //     });
+    //     return mostrar;
+    // };
+    //
+    //
+    this.eliminarRecursos = function (id_agente, id_recurso){
+        var eliminar = agente[id_agente].recursos.map(e => e.id).indexOf(id_recurso);
+        // console.log(eliminar);
+         agente[id_agente].recursos.splice(eliminar,1);
+         return agente;
+        // localStorage.setItem('agentes',JSON.stringify(agente));
+      // });
+    }
 
 
 }
@@ -70,5 +77,5 @@ var recursos = function(){
 
 
 if(typeof exports !== 'undefined') {
-  exports.ejecutar = ejecutar;
+  exports.recurso = recurso;
 }
--- assets/test/test.js
+++ assets/test/test.js
@@ -1,34 +1,82 @@
 var assert    = require('assert');
-var appTest = require('main_test.js');
+var appTest = require('../test/main_test.js');
 
 describe("Resource app test",function() {
   var app;
   before(function(){
     app = new appTest.recurso();
   });
-  //{ id : 0 , tipo: 'fisico', url : 'bjstdmngbgr01.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.2' , directorio : '/var/lib/cruise-agent', recursos : ['Ubuntu','firefox','core-duo']},
-
-  it('Debería agregar un recurso',function(){
-    // assert.equal(app.hola(),"Hola");
-  });
 
   it('Debería agregar 3 recursos',function(){
-
+    app.Agente();
+    assert.deepEqual(app.agregarRecursos(2, 'core-duo, Mysql, MongoDB'), { id : 2 , tipo: 'fisico', url : 'bjstdmngbgr03.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.4' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}, {id: 'R1', nombre: 'Mysql'}, {id: 'R2', nombre: 'MongoDB'}]});
+  });
+  it('Debería agregar un recurso',function(){
+    app.Agente();
+    assert.deepEqual(app.agregarRecursos(1,'Mysql'),{ id : 1 , tipo: 'fisico', url : 'bjstdmngbgr02.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.3' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}, {id: 'R1', nombre: 'Mysql'}]});
   });
 
-  it('Debería ver los tipos de agente y contar',function(){
 
+  it('Debería ver los tipos de agente y contar',function(){
+    assert.equal(app.summary('idle', [{ id : 0 , tipo: 'fisico', url : 'bjstdmngbgr01.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.2' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'},{id: 'R1', nombre: 'firefox'},]},
+    { id : 1 , tipo: 'fisico', url : 'bjstdmngbgr02.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.3' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+    { id : 2 , tipo: 'fisico', url : 'bjstdmngbgr03.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.4' , directorio : '/var/lib/cruise-agent', recursos : []},
+    { id : 3 , tipo: 'fisico', url : 'bjstdmngbgr04.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.5' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+    { id : 4 , tipo: 'virtual', url : 'bjstdmngbgr05.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.6' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'firefox'}]},
+    { id : 5 , tipo: 'virtual', url : 'bjstdmngbgr06.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.7' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]},
+    { id : 6 , tipo: 'virtual', url : 'bjstdmngbgr07.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.8' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]}]), 3);
   });
 
   it('Debería poder eliminar un recurso',function(){
-
+    app.Agente();
+      assert.deepEqual(app.eliminarRecursos(0, 'R1'),[{ id : 0 , tipo: 'fisico', url : 'bjstdmngbgr01.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.2' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+      { id : 1 , tipo: 'fisico', url : 'bjstdmngbgr02.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.3' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+      { id : 2 , tipo: 'fisico', url : 'bjstdmngbgr03.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.4' , directorio : '/var/lib/cruise-agent', recursos : []},
+      { id : 3 , tipo: 'fisico', url : 'bjstdmngbgr04.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.5' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+      { id : 4 , tipo: 'virtual', url : 'bjstdmngbgr05.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.6' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'firefox'}]},
+      { id : 5 , tipo: 'virtual', url : 'bjstdmngbgr06.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.7' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]},
+      { id : 6 , tipo: 'virtual', url : 'bjstdmngbgr07.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.8' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]}
+      ]);
   });
 
   it('Debería poder eliminar 3 recursos',function(){
+    app.Agente();
+    var x = app.agregarRecursos(1,'MySQL ');
+    var y = app.agregarRecursos(2,'MySQL, Ubuntu, core-duo');
+    var z = app.eliminarRecursos(2, 'R2');
+    var a = app.eliminarRecursos(2, 'R0');
+
+    assert.deepEqual(app.eliminarRecursos(2, 'R1'), [{ id : 0 , tipo: 'fisico', url : 'bjstdmngbgr01.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.2' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'},{id: 'R1', nombre: 'firefox'}]},
+    { id : 1 , tipo: 'fisico', url : 'bjstdmngbgr02.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.3' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'},{id: 'R1', nombre: 'MySQL'}]},
+    { id : 2 , tipo: 'fisico', url : 'bjstdmngbgr03.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.4' , directorio : '/var/lib/cruise-agent', recursos : []},
+    { id : 3 , tipo: 'fisico', url : 'bjstdmngbgr04.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.5' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+    { id : 4 , tipo: 'virtual', url : 'bjstdmngbgr05.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.6' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'firefox'}]},
+    { id : 5 , tipo: 'virtual', url : 'bjstdmngbgr06.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.7' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]},
+    { id : 6 , tipo: 'virtual', url : 'bjstdmngbgr07.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.8' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]}]);
 
   });
 
-  it('Debería actualizar el localStorage',function(){
+  it('Debería mostrar de acuerdo al tipo',function(){
+      app.Agente();
+      assert.deepEqual(app.mostrarAgentes('fisico'),[{ id : 0 , tipo: 'fisico', url : 'bjstdmngbgr01.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.2' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'},{id: 'R1', nombre: 'firefox'}]},
+      { id : 1 , tipo: 'fisico', url : 'bjstdmngbgr02.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.3' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+      { id : 2 , tipo: 'fisico', url : 'bjstdmngbgr03.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.4' , directorio : '/var/lib/cruise-agent', recursos : []},
+      { id : 3 , tipo: 'fisico', url : 'bjstdmngbgr04.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.5' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]}
+      ]);
+
+      assert.deepEqual(app.mostrarAgentes('virtual'),[{ id : 4 , tipo: 'virtual', url : 'bjstdmngbgr05.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.6' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'firefox'}]},
+      { id : 5 , tipo: 'virtual', url : 'bjstdmngbgr06.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.7' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]},
+      { id : 6 , tipo: 'virtual', url : 'bjstdmngbgr07.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.8' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]}
+      ]);
+
+      assert.deepEqual(app.mostrarAgentes('all'),[{ id : 0 , tipo: 'fisico', url : 'bjstdmngbgr01.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.2' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'},{id: 'R1', nombre: 'firefox'}]},
+      { id : 1 , tipo: 'fisico', url : 'bjstdmngbgr02.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.3' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+      { id : 2 , tipo: 'fisico', url : 'bjstdmngbgr03.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.4' , directorio : '/var/lib/cruise-agent', recursos : []},
+      { id : 3 , tipo: 'fisico', url : 'bjstdmngbgr04.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.5' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'Ubuntu'}]},
+      { id : 4 , tipo: 'virtual', url : 'bjstdmngbgr05.thoughtworks.com' , estado : 'idle' , direccionIP: '192.168.1.6' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'firefox'}]},
+      { id : 5 , tipo: 'virtual', url : 'bjstdmngbgr06.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.7' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]},
+      { id : 6 , tipo: 'virtual', url : 'bjstdmngbgr07.thoughtworks.com' , estado : 'building' , direccionIP: '192.168.1.8' , directorio : '/var/lib/cruise-agent', recursos : [{id: 'R0', nombre: 'core-duo'}]}
+      ]);
 
   });
 });
